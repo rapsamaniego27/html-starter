b294633efccdca484188b59793e79c486e1e5b4e
Ralph Samaniego <rapsamaniego27@gmail.com>
2019-09-13 10:53:31 +0800

    added padding and margins

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ function scssTask(){
         .pipe(sourcemaps.init()) // initialize sourcemaps first
         .pipe(sass()) // compile SCSS to CSS
         .pipe(concat('style.min.css'))
-        .pipe(postcss([ cssnano(), autoprefixer({ overrideBrowserslist: ['IE 6','Chrome 9', 'Firefox 14']}) ])) // PostCSS plugins
+        // .pipe(postcss([ cssnano(), autoprefixer({ overrideBrowserslist: ['IE 6','Chrome 9', 'Firefox 14']}) ])) // PostCSS plugins
         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory 
         .pipe(dest('dist')
     ); // put final CSS in dist folder
